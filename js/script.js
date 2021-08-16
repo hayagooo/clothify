a6e1651c61046f2ce562f68422b747d6f65fd82d
Ilhami magfiro <ilhammaghfiro@gmail.com>
2021-08-17 04:25:41 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,327 @@
+const baju = [{
+        id: 1,
+        nama: 'Baju 1',
+        sisa: 6,
+        ukuran: 'XL',
+        terjual: 300,
+        gambar: 'baju-1.jpeg',
+        harga: 125
+    },
+    {
+        id: 2,
+        nama: 'Baju 2',
+        sisa: 4,
+        terjual: 290,
+        ukuran: 'XL',
+        gambar: 'baju-2.jpeg',
+        harga: 100
+    },
+    {
+        id: 3,
+        nama: 'Baju 3',
+        sisa: 4,
+        terjual: 290,
+        ukuran: 'XL',
+        gambar: 'baju-3.jpeg',
+        harga: 100
+    },
+    {
+        id: 4,
+        nama: 'Baju 4',
+        sisa: 4,
+        terjual: 290,
+        ukuran: 'XL',
+        gambar: 'baju-4.jpeg',
+        harga: 100
+    },
+    {
+        id: 5,
+        nama: 'Baju 5',
+        sisa: 4,
+        terjual: 290,
+        ukuran: 'XL',
+        gambar: 'baju-5.jpeg',
+        harga: 100
+    },
+    {
+        id: 6,
+        nama: 'Baju 6',
+        sisa: 4,
+        terjual: 290,
+        gambar: 'baju-6.jpeg',
+        harga: 100
+    },
+    {
+        id: 7,
+        nama: 'Baju 7',
+        sisa: 4,
+        terjual: 290,
+        gambar: 'baju-7.jpeg',
+        harga: 100
+    },
+    {
+        id: 8,
+        nama: 'Baju 8',
+        sisa: 4,
+        terjual: 290,
+        gambar: 'baju-8.jpeg',
+        harga: 100
+    },
+    {
+        id: 9,
+        nama: 'Baju 9',
+        sisa: 4,
+        terjual: 290,
+        gambar: 'baju-9.jpeg',
+        harga: 100
+    },
+]
+
+const celana = [{
+        id: 1,
+        nama: 'Celana 1',
+        sisa: 100,
+        terjual: 300,
+        ukuran: 33,
+        gambar: 'celana-1.jpeg',
+        harga: 125
+    },
+    {
+        id: 2,
+        nama: 'Celana 2',
+        sisa: 4,
+        ukuran: 40,
+        terjual: 290,
+        gambar: 'celana-2.jpeg',
+        harga: 100
+    },
+    {
+        id: 3,
+        nama: 'Celana 3',
+        sisa: 4,
+        ukuran: 40,
+        terjual: 290,
+        gambar: 'celana-3.jpeg',
+        harga: 100
+    },
+    {
+        id: 4,
+        nama: 'Celana 4',
+        sisa: 4,
+        ukuran: 40,
+        terjual: 290,
+        gambar: 'celana-4.jpeg',
+        harga: 100
+    },
+    {
+        id: 5,
+        nama: 'Celana 5',
+        sisa: 4,
+        ukuran: 40,
+        terjual: 290,
+        gambar: 'celana-5.jpeg',
+        harga: 100
+    },
+    {
+        id: 6,
+        nama: 'Celana 6',
+        sisa: 4,
+        ukuran: 40,
+        terjual: 290,
+        gambar: 'celana-6.jpeg',
+        harga: 100
+    },
+    {
+        id: 7,
+        nama: 'Celana 7',
+        sisa: 4,
+        ukuran: 40,
+        terjual: 290,
+        gambar: 'celana-7.jpeg',
+        harga: 100
+    },
+    {
+        id: 8,
+        nama: 'Celana 8',
+        sisa: 4,
+        ukuran: 40,
+        terjual: 290,
+        gambar: 'celana-8.jpeg',
+        harga: 100
+    },
+    {
+        id: 9,
+        nama: 'Celana 9',
+        sisa: 4,
+        ukuran: 40,
+        terjual: 290,
+        gambar: 'celana-9.jpeg',
+        harga: 100
+    },
+    {
+        id: 10,
+        nama: 'Celana 10',
+        sisa: 4,
+        ukuran: 40,
+        terjual: 290,
+        gambar: 'celana-10.jpeg',
+        harga: 100
+    },
+    {
+        id: 11,
+        nama: 'Celana 11',
+        sisa: 4,
+        ukuran: 40,
+        terjual: 290,
+        gambar: 'celana-11.jpeg',
+        harga: 100
+    },
+
+]
+
+const sepatu = [{
+    id: 1,
+    nama: 'Sepatu 1',
+    sisa: 4,
+    terjual: 290,
+    ukuran: 42,
+    gambar: 'sepatu-1.jpeg',
+    harga: 100
+}, {
+    id: 2,
+    nama: 'Sepatu 2',
+    sisa: 4,
+    terjual: 290,
+    ukuran: 42,
+    gambar: 'sepatu-2.jpeg',
+    harga: 100
+}, {
+    id: 3,
+    nama: 'Sepatu 3',
+    sisa: 4,
+    terjual: 290,
+    ukuran: 42,
+    gambar: 'sepatu-3.jpeg',
+    harga: 100
+}, {
+    id: 4,
+    nama: 'Sepatu 4',
+    sisa: 4,
+    terjual: 290,
+    ukuran: 42,
+    gambar: 'sepatu-4.jpeg',
+    harga: 100
+}, {
+    id: 5,
+    nama: 'Sepatu 5',
+    sisa: 4,
+    terjual: 290,
+    ukuran: 42,
+    gambar: 'sepatu-5.jpeg',
+    harga: 100
+}, {
+    id: 6,
+    nama: 'Sepatu 6',
+    sisa: 4,
+    terjual: 290,
+    ukuran: 42,
+    gambar: 'sepatu-6.jpeg',
+    harga: 100
+}, {
+    id: 7,
+    nama: 'Sepatu 7',
+    sisa: 4,
+    terjual: 290,
+    ukuran: 42,
+    gambar: 'sepatu-7.jpeg',
+    harga: 100
+}]
+
+const popular = [{
+        id: 1,
+        nama: 'Baju 1',
+        sisa: 6,
+        ukuran: 'XL',
+        terjual: 300,
+        gambar: 'baju-1.jpeg',
+        harga: 125
+    },
+    {
+        id: 2,
+        nama: 'Baju 2',
+        sisa: 4,
+        terjual: 290,
+        ukuran: 'XL',
+        gambar: 'baju-2.jpeg',
+        harga: 100
+    }, {
+        id: 3,
+        nama: 'Celana 4',
+        sisa: 4,
+        ukuran: 40,
+        terjual: 290,
+        gambar: 'celana-4.jpeg',
+        harga: 100
+    }, {
+        id: 4,
+        nama: 'Celana 5',
+        sisa: 4,
+        ukuran: 40,
+        terjual: 290,
+        gambar: 'celana-5.jpeg',
+        harga: 100
+    }, {
+        id: 5,
+        nama: 'Sepatu 3',
+        sisa: 4,
+        terjual: 290,
+        ukuran: 42,
+        gambar: 'sepatu-3.jpeg',
+        harga: 100
+    }, {
+        id: 6,
+        nama: 'Sepatu 4',
+        sisa: 4,
+        terjual: 290,
+        ukuran: 42,
+        gambar: 'sepatu-4.jpeg',
+        harga: 100
+    },
+]
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        name: 'ok',
+        product: baju,
+        productActive: 'Baju',
+        keyword: '',
+        message: '',
+        popular: popular
+
+    },
+    methods: {
+        changeProduct: function (product) {
+            this.productActive = product
+            this.product = eval(this.productActive.toLowerCase())
+
+        },
+        search: function () {
+            this.product = eval(this.productActive.toLowerCase())
+            const keyword = this.keyword
+            const result = this.product.filter(item => item.nama == keyword)
+            if (result.length >= 1) {
+                if (keyword != '') {
+                    this.product = result
+                    this.message = ''
+                }
+            }
+            if (result.length < 1) {
+                this.message = 'Not Found'
+                this.product = []
+            }
+
+        }
+
+    }
+})
